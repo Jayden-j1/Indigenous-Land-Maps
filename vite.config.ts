ece33200157d3fd3b8741d40dc6a91aa466971af
Jayden Jacky <184793126+Jayden-j1@users.noreply.github.com>
2025-12-11 21:56:42 +1100

    fix: correct Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/indigenous-land-maps/",
+  base: "/Indigenous-Land-Maps/",
   plugins: [
     react(),
     tailwindcss(),
